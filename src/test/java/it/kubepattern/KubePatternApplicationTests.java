532f2132b6ca873c2dcf817d013c5dde84cbfb91
Gabriele <gabriele.groppo@sigemi.it>
2025-12-04 14:41:20 +0100

    startup project

---
+++ src/test/java/it/kubepattern/KubePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package it.kubepattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubePatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
